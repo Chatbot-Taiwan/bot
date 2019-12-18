cd1c4bb4878a7607aa280006847d30d7fae13372
Eric <smart032410@gmail.com>
2019-12-18 14:27:24 +0800

    Add home

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 const { router, text } = require('bottender/router');
 const { NewMeetupInfo, MeetupHistory } = require('./meetup');
+const { Home } = require('./home');
 
 export default async function App(context: any) {
   if (context.event.isText) {
     return router([
+        text('主選單', Home),
         text('最新小聚', NewMeetupInfo),
         text('歷史小聚', MeetupHistory),
       ]);
